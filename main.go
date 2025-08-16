4a4b83d5dc23e377bc479142e053e3c586c41b1e
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2025-08-16 23:15:31 +0300

    added base structure for handling cli calls

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"test-task/logger"
 	"test-task/task"
@@ -24,4 +26,40 @@ func main() {
 
 	isFileSystemInit := task.FsInit()
 	logger.LogVerbose(false, "Is program file system ready: %t", isFileSystemInit)
+
+	if len(os.Args) > 1 {
+		printHelp()
+		return
+	}
+
+	switch os.Args[1] {
+	case "add":
+		break
+
+	case "update":
+		break
+
+	case "delete":
+		break
+
+	case "mark":
+		break
+
+	case "list":
+		break
+
+	default:
+		printHelp()
+	}
+
+}
+
+func printHelp() {
+	fmt.Println("Use this commands to access task-cli tool:")
+	fmt.Println("  add [description]          				Add new task with description")
+	fmt.Println("  update [id] [description, status]  Update task, can use without status argument")
+	fmt.Println("  delete [id]                     		Delete task")
+	fmt.Println("  mark in-progress [id]           		Update status to in-progress")
+	fmt.Println("  mark done [id]                  		Update status to done")
+	fmt.Println("  list empty/done/todo/in-progress   List all task or specified by status (status: todo, in-progress, done)")
 }
